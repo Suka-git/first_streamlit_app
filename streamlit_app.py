9125e53475f1758d1f283df7e3b929951840b5d4
Suka-git <55882792+Suka-git@users.noreply.github.com>
2023-07-04 20:06:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner Breakfast Lunch Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal'🥣 )
-streamlit.text('Kale, Spinach & Rocket Smoothie'🥗 🥑🍞)
-streamlit.text('Hard-Boiled Free-Range Egg' 🐔)
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣' )
+streamlit.text('Kale, Spinach & Rocket Smoothie🥗 🥑🍞')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔 ')
    
 
