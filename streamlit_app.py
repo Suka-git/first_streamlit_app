5a62b7c96251b99551cc2366e9305a46febd3c52
Suka-git <55882792+Suka-git@users.noreply.github.com>
2023-07-04 20:07:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣' )
 streamlit.text('Kale, Spinach & Rocket Smoothie🥗 🥑🍞')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔 ')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
 
