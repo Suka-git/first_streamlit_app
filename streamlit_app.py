bee126ceeb065861bb5af9ea63d00212bfae708c
Suka-git <55882792+Suka-git@users.noreply.github.com>
2023-07-09 18:35:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #create repeatable code called function 
  def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_ruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      return (fruityvice_normalized) 
 #add text entry box
 try:
