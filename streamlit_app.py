9200faff07c87ea9af993cfc12e13fb7836c700a
Suka-git <55882792+Suka-git@users.noreply.github.com>
2023-07-09 18:22:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,17 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #add text entry box
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# create new table structure 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display data in table
 streamlit.dataframe(fruityvice_normalized)
 
+except URLError as e:
+streamlit.error()
 streamlit.stop()
 #connect to snowflake  
 
